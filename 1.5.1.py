757ee83e5ab442d60880e8d6322bbe87e9434377
shв <shalyutindv@gmail.com>
2018-10-07 23:42:26 +0300

    task compleet

--- 1.5.1.py
+++ 1.5.1.py
@@ -1,8 +1,16 @@
 nums = [1, 5, 2, 7, 1, 9, 3, 8, 5, 9]
 a=0
-i = 1
-while i <=len(nums):
+i = 0
+while i < len(nums):
     a=a+nums[i]
     i = i + 1
 b=a/len(nums)
---
+#print(b)
+c = 0
+i = 0
+while i < len(nums):
+    c=c+(nums[i]-b)**2
+    i = i + 1
+b=c/len(nums)-1
+d=b**(1/2)
+print ("d=" + d)
