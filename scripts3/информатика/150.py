73586791bfdc6c66d4df53cc51e539063967b384
shd2016 <shalyutindv@gmail.com>
2018-11-01 00:59:51 +0300

    --

--- scripts3/информатика/150.py
+++ scripts3/информатика/150.py
@@ -1,14 +1,17 @@
-n=int(input ('введите число '))
+n=int(input ())
 find=False
 if n<=1028:
-    for x in range (0,11):
+    for x in range (0,12):
         i=x**3
-        m=(n-i)**(1/3)
-        if m.is_integer():
-            print (int(m),x)
-            find=True
+        if n-i < 0:
             break
+        m=(n-i)**(1/3)
+        print(i,m,x)
+        #if not isinstance(m, complex):
+        if m.is_integer():
+            if int(m)**3 + x**3 == n :
+                print (int(m),x)
+                find=True
+                break
     if not find :    
         print ('impossible')
-else:
-    print ('введите правильные числа')
