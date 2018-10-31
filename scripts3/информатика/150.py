d78b6dc2878f74e9101a14aafcc5b283213a96ce
shd2016 <shalyutindv@gmail.com>
2018-10-31 15:31:59 +0300

    v02 - with some errors, but in the end program's answer is right

--- scripts3/информатика/150.py
+++ scripts3/информатика/150.py
@@ -1,12 +1,12 @@
 n=int(input ('введите число '))
 m=0
 f=0
-i=x**3
 if n<=1028:
     for x in range (0,10):
+        i=x**3
         m=(n-i)**(1/3)
         f=round(m)
-        if m=f:
+        if m==f:
             print (m,i)
         else:
             print ('inpossible')
