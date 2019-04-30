0e7db3946a5098e94c190bcc0abef39db3b8797f
ruasld1 <DSHALYUTIN@raiffeisen.ru>
2019-04-30 18:56:31 +0300

    Смена рабочего каталога для IDLE

---
+++ init.py
@@ -0,0 +1,5 @@
+#"c:\Users\ruasld1\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\IDLE (Python 3.7 64-bit).lnk"  -r d://git//hello//init.py
+# c:\Users\ruasld1\AppData\Local\Programs\Python\Python37>python -m pip install numpy
+import os
+os.chdir('d://git//hello')
+#print(os.getcwd())
